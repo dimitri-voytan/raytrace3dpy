dc65958c3a71b2384f3ce5a6abdf7a5af0c69265
dimitrivoytan <dimitrivoytan@gmail.com>
2022-08-03 10:07:25 -0500

    Correctly access slowness for grad estimate

--- src/beam3D.py
+++ src/beam3D.py
@@ -105,7 +105,7 @@ class OnePointBeam3D():
         '''
         Simple central difference to approximate the gradient.
         '''
-        x, y, z = [int(item) for item in coord]
+        x, y, z = [item for item in coord]
         # Could replace with a higher order scheme?
         ds_dx = (s((x+self.dx, y, z))-s((x-self.dx, y, z)))/(2*self.dx)
         ds_dy = (s((x, y+self.dy, z))-s((x, y-self.dy, z)))/(2*self.dy)
